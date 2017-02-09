c2ef03a91651190b6dfd86da8ef237d1fc07503b
TheSirC <clocloranely@hotmail.fr>
2017-02-09 11:26:43 +0100

    Commit initial

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+extern crate json;
+
+fn affichage() {
+    // Affiche l'aide si les options sont --help ou si elles ne contiennent pas --account ou --page et g�re les exceptions, renvoie les arguments passées au programme
+    unimplemented!()
+}
+
+fn main() {
+    (compte, nbpages,limite) = affichage();
+    worker
+}
