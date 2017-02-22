1e560c838e17a0bc83b59a7dd05a38e7f3190397
TheSirC <clocloranely@hotmail.fr>
2017-02-22 15:40:58 +0100

    Mis à jour du test
    Changement du test vis-à-vis du commit précédent ( toujours non fonctionnel par contre)

--- tests/json.rs
+++ tests/json.rs
@@ -4,21 +4,27 @@ extern crate scraper;
 
 use hyper::client::Client;
 use libsnatch::client::GetResponse;
+use std::io::Read;
+use scraper::{Html, Selector};
 
 
 #[test]
-fn test_name() {
+fn json_parsing() {
     let hyper_client = Client::new();
     let host = "hypem.com".to_string();
     let url: String = "http://".to_string() + &host;
     // Get informations from arguments
     let account = "SirC".to_string(); // The use of unwrap is legit here because the argument must be entered
     let page = "1".to_string(); // The use of unwrap is legit here because the argument must be entered
+    let mut html_page_content = String::new();
     let html_page = hyper_client.get_http_response(&(format!("{}/{}/{}", &url, &account, &page)))
-        .expect("The server didn't answer");
+        .expect("The server didn't answer")
+        .read_to_string(&mut html_page_content);
     // Create the parser
     let json_class = Selector::parse("#displayList-data").expect("Initializing the parsing failed");
+    // Creating a parsing fragment (for it to live long enough)
+    let parser = Html::parse_fragment(&html_page_content);
     // Parsing the DOM to find the json
-    let json = Html::parse_fragment(html_page).select(&json_class).expect("Parsing failed");
+    let json = parser.select(&json_class);
     assert!(json)
 }
